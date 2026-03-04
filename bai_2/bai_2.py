5892e2e65f7ae4684c44c6dc486d5e7472118496
Nguyễn Tiến Đạt <datn2326@gmail.com>
2026-03-04 19:51:38 +0700

    bai 2

---
+++ bai_2/bai_2.py
@@ -0,0 +1,4 @@
+chieu_dai = float(input(" + Nhập chiều dài: "))
+chieu_rong = float(input(" + Nhập chiều rộng: "))
+dien_tich = chieu_dai * chieu_rong
+print(f"==> Diện tích hình chữ nhật là: {dien_tich}")
