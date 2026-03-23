c1e6e7d4de1d006e1030ee3511345c87a8c2daab
Nguyễn Tiến Đạt <datn2326@gmail.com>
2026-03-23 17:26:26 +0700

    bai 20

---
+++ bai_19/bai_19.py
@@ -0,0 +1,11 @@
+numbers_list = [10, 25, 5, 40, 30]
+max_value = max(numbers_list)
+min_value = min(numbers_list)
+#Tính trung bình cộng
+total_sum = sum(numbers_list)
+count = len(numbers_list)
+average = total_sum / count
+print("Danh sách số:", numbers_list)
+print("Số lớn nhất (Max) là:", max_value)
+print("Số nhỏ nhất (Min) là:", min_value)
+print("Giá trị trung bình (Average) là:", average)
